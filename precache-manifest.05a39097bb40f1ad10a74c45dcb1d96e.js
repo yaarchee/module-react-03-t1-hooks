528dfd35c967c6d99c21c1a8a908f3e705db123e
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-09-29 00:41:49 +0300

    Updates

---
+++ precache-manifest.05a39097bb40f1ad10a74c45dcb1d96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d9f42b14f2df9ed3baceb9d1360635",
+    "url": "/module-react-03-t1-hooks/index.html"
+  },
+  {
+    "revision": "563649cf5075ed135401",
+    "url": "/module-react-03-t1-hooks/static/css/main.d8005fd2.chunk.css"
+  },
+  {
+    "revision": "d34a9f07bdcbe51a8661",
+    "url": "/module-react-03-t1-hooks/static/js/2.3d5db69b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/module-react-03-t1-hooks/static/js/2.3d5db69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563649cf5075ed135401",
+    "url": "/module-react-03-t1-hooks/static/js/main.bd60bc10.chunk.js"
+  },
+  {
+    "revision": "ac035b92f7ece81f7def",
+    "url": "/module-react-03-t1-hooks/static/js/runtime-main.93e789bd.js"
+  }
+]);
